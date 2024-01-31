6021a3612c1c2cfb5de2d58c7ec4fef2e4bd2a79
gao8a <rikgao@icloud.com>
2024-01-30 23:43:55 -0500

    edit readme

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,11 @@
 "use client";
 
-import Map, { Layer, Source } from "react-map-gl";
-import { useState, useEffect, useMemo, useCallback, useRef } from 'react';
+import { useEffect } from 'react';
 import "mapbox-gl/dist/mapbox-gl.css";
-import NavBar from "./ui/navbar";
-import PM25Chart from "./ui/pm25barchart";
 import ThemeClient from "./ui/themeClient";
-import BiaxialLineChart from "./ui/dailylinechart";
 import { useRouter } from 'next/navigation'
-import { Box, Container } from '@mui/system';
-import Paper from '@mui/material/Paper';
-import type { RasterLayer } from 'react-map-gl';
-import mapboxgl from 'mapbox-gl';
-import type { GeoJSONSource } from 'mapbox-gl';
-import TextField from '@mui/material/TextField';
-
-
-import settlements from "../public/data/settlements.json";
 import classes from "./page.module.css";
-import { FeatureCollection } from "geojson";
+
 
 
 export default function Home() {
